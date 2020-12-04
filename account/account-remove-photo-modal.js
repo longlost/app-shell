6aa9a5b90bacf93aed3caf8c6c3966c5070f8be8
Clay Bennington <claymation296@gmail.com>
2020-12-04 14:10:47 -0700

    Continued work on account photo management. This version includes initial logic for setting a new account photo. Updated deps.

---
+++ account/account-remove-photo-modal.js
@@ -0,0 +1,67 @@
+
+/**
+  * 
+  *  `account-remove-photo-modal`
+  *
+  *
+  *  Confirmation modal for removing a photo from the user's account profile.
+  *
+  *
+  *  @customElement
+  *  @polymer
+  *  @demo demo/index.html
+  *
+  *
+  **/
+
+import {AppElement, html} from '@longlost/app-element/app-element.js';
+import htmlString 				from './account-remove-photo-modal.html';
+import '@longlost/app-overlays/app-modal.js';
+import '@polymer/paper-button/paper-button.js';
+
+
+class AccountRemovePhotoModal extends AppElement {
+  static get is() { return 'account-remove-photo-modal'; }
+
+  static get template() {
+    return html([htmlString]);
+  }
+
+
+  async __dismissButtonClicked() {
+    try {
+      await this.clicked();
+      await this.close();
+    }
+    catch (error) {
+      if (error === 'click debounced') { return; }
+      console.error(error);
+    }
+  }
+
+
+  async __deleteButtonClicked() {
+    try {
+      await this.clicked();
+      await this.close();
+      this.fire('remove-photo-modal-remove');
+    }
+    catch (error) {
+      if (error === 'click debounced') { return; }
+      console.error(error);
+    }
+  }
+
+
+  close() {
+    return this.$.modal.close();
+  }
+
+
+  open() {
+    return this.$.modal.open();
+  }
+  
+}
+
+window.customElements.define(AccountRemovePhotoModal.is, AccountRemovePhotoModal);
--- boot/cloud.js
+++ boot/cloud.js
@@ -54,10 +54,107 @@ exports.createUser = functions.auth.user().onCreate(async user => {
   return null; 
 });
 
+
 // Remove user info from db when user account is deleted.
 exports.deleteUser = functions.auth.user().onDelete(async user => {
-	
+  
   await admin.firestore().collection('users').doc(user.uid).delete(); 
 
   return null;  
 });
+
+
+const imageProcessingDone = item => {
+  const {optimized, optimizedError, thumbnail, thumbnailError} = item;
+  const optimizedDone = Boolean(optimized || optimizedError);
+  const thumbnailDone = Boolean(optimized || optimizedError);
+
+  return optimizedDone && thumbnailDone;
+};
+
+// Wait for image to finish being processed, then 
+// save the updates to the user profile data.
+const manageProfilePhoto = async (item, type, ref, userId) => {
+  if (!item || imageProcessingDone(item)) { return; }
+
+  const {coll, doc, uid} = item;
+
+  return new Promise((resolve, reject) => {
+
+    // Get live updates on the photo item being processed.
+    // Subscribe to the db location where photo is being saved.
+    const unsubscribe = await admin.firestore().collection(coll).doc(doc).
+      onSnapshot(async snap => {
+
+        // Bail if the item has been deleted before processing is done.
+        if (!snap.exists) { 
+          unsubscribe();
+          resolve();
+          return;
+        }
+
+        const data = snap.data();
+
+        // Skip partially processed snapshots.
+        if (!imageProcessingDone(data)) { return; }
+
+        // Double check the 'users/{userId}' data to make sure
+        // that the user hasn't changed the photo again.
+        const doubleCheckData = await admin.firestore().collection('users').doc(userId).get();
+
+        // Test 'uid' against the returned 'data[type].uid'.
+        // Unsubscribe and resolve the promise if they are not equal.
+        if (doubleCheckData[type].uid !== uid) {
+          unsubscribe();
+          resolve();
+          return;
+        }
+
+        unsubscribe();
+
+        const {optimized, thumbnail} = data;
+        const photoURL = thumbnail || optimized;
+
+        // Update Firestore 'users/{userId}' ref 
+        // and Auth User photoURL field.
+        await Promise.all([
+          ref.set({[type]: data}, {merge: true}),
+          admin.auth().updateUser(userId, {photoURL})
+        ]);
+
+        resolve();
+        
+      }, reject);
+  });  
+};
+
+
+// Manage user profile photos.
+// Waits for image processing to complete then updates user data.
+//
+// This implementation provides a better user experience since
+// the client is not forcing the user to wait while cloud processing
+// is taking place.
+//
+// This also makes for a more reliable mechanism since the client can
+// go offline or be refreshed at any time during processing. 
+exports.updateProfilePhotos = functions.firestore.
+  document('users/{userId}').
+  onUpdate(async (change, context) => {
+
+    const data = change.after.data();
+    const ref  = change.after.ref;
+
+    const {avatar, background} = data;
+    const {userId}             = context;
+
+    // No profile photo changes to manage, so bail.
+    if (!avatar && !background) { return null; }
+
+    await Promise.all([
+      manageProfilePhoto(avatar,     'avatar',     ref, userId),
+      manageProfilePhoto(background, 'background', ref, userId)
+    ]);
+
+    return null;
+  });
